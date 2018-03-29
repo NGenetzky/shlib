7d616fc634b535a2b76aa1419b4c17ef3fa9dc2d
Nathan Genetzky <nathan.genetzky@milestone.com>
2018-03-29 14:17:32 -0500

    commands/sshfs_mnt_rootfs

---
+++ commands/sshfs_mnt_rootfs.sh
@@ -0,0 +1,17 @@
+
+sshfs_mnt_rootfs()
+{
+    local ip=${1?'Specify the IP of the device'}
+    local mnt_path="/mnt/${ip?}/"
+
+    # return success if already mounted.
+    mountpoint -q "${mnt_path}" && return 0
+
+    # Otherwise ensure the directory exists.
+    [ -d "${mnt_path}" ] || sudo install -d -o "$(id -u)" -g "$(id -g)" "${mnt_path}"
+
+    # and mount it.
+    sshfs "root@${ip}:/" "${mnt_path}"
+    return $?
+}
+
