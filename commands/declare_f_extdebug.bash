48abbc3ca3ca8ece8c2c6c172fcb9edb90483201
Nathan Genetzky <nathan.genetzky@milestone.com>
2018-06-22 16:17:50 -0500

    commands/declare_f_extdebug

---
+++ commands/declare_f_extdebug.bash
@@ -0,0 +1,13 @@
+
+declare_f_extdebug()
+{
+    # https://superuser.com/a/974066
+
+    # Turn on extended shell debugging
+    shopt -s extdebug
+    # Display the function’s name, line number and fully qualified source file
+    declare -F ${1?"1:Specify name of function"}
+    # Turn off extended shell debugging
+    shopt -u extdebug
+}
+
