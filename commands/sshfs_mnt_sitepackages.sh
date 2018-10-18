c42147a0202815a3f16e64315f4c862d852658ad
Nathan Genetzky <nathan.genetzky@milestone.com>
2018-10-18 15:26:31 -0500

    commands: sshfs_mnt_sitepackages

---
+++ commands/sshfs_mnt_sitepackages.sh
@@ -0,0 +1,17 @@
+
+sshfs_mnt_sitepackages()
+{
+    local ip=${1?'Specify the IP of the device'}
+    local mnt_path="/mnt/site-packages-${ip?}/"
+
+    # return success if already mounted.
+    mountpoint -q "${mnt_path}" && return 0
+
+    # Otherwise ensure the directory exists.
+    [ -d "${mnt_path}" ] || sudo install -d -o "$(id -u)" -g "$(id -g)" "${mnt_path}"
+
+    # and mount it.
+    sshfs "root@${ip}:/usr/lib/python2.7/site-packages/" "${mnt_path}"
+    return $?
+}
+
