a4058094df57a206813b84b76e6aefb63d69a826
Nathan Genetzky <nathan@genetzky.us>
2018-11-16 18:55:47 -0600

    snippet/main: Basic structure of bash script

---
+++ snippets/main.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+
+main(){
+  local args=$@
+  echo ${args}
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    # set -x
+    main "$@"
+fi
+
