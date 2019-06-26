12f9eeac1b67d0c36f2f01c6aef5ed6ce648657f
Nathan Genetzky <nathan@genetzky.us>
2019-06-25 23:19:10 -0500

    shellcheck: Fix apt-purge-unused-kernels.bash

--- tool/apt/apt-purge-unused-kernels.bash
+++ tool/apt/apt-purge-unused-kernels.bash
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 apt_purse_unused_kernels(){
     # Reference:
     # http://tuxtweaks.com/2010/10/remove-old-kernels-in-ubuntu-with-one-command/
     # Source:
     # https://askubuntu.com/a/259092
-    dpkg -l linux-{image,headers}-"[0-9]*" | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e '[0-9]' | xargs sudo apt-get -y purge
+    dpkg -l linux-{image,headers}-"[0-9]*" | awk '/^ii/{ print $2}' | grep -v -e "$(uname -r | cut -f1,2 -d"-")" | grep -e '[0-9]' | xargs sudo apt-get -y purge
 }
 
 apt_purse_unused_kernels
