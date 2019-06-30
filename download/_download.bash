97e14a040e0089856a66cede24d6343f2abd9794
Nathan Genetzky <nathan@genetzky.us>
2019-06-30 17:54:43 -0500

    download: directory of helpers for downloading from web

---
+++ download/_download.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+readonly DL_DIR="${HOME}/Downloads/"
+readonly BIN_DIR="${HOME}/.local/bin/"
+
+wget_download(){
+    local url="${1?}"
+    local name="${2?}"
+
+    local path="${DL_DIR}/${name}"
+    wget -O "${path}" "${url}"
+}
+
+wget_download_install_executable(){
+    local url="${1?}"
+    local name="${2?}"
+
+    local path="${BIN_DIR}/${name}"
+    wget -O "${path}" "${url}"
+    chmod 755 "${path}"
+}
