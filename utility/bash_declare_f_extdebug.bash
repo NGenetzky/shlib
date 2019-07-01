c02f5d22c807c4ca213e7e8ec4f9ced75766d940
Nathan Genetzky <nathan@genetzky.us>
2019-07-01 00:55:56 -0500

    utility: Improve bash_declare_f_extdebug.bash

--- utility/bash_declare_f_extdebug.bash
+++ utility/bash_declare_f_extdebug.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-declare_f_extdebug()
+bash_declare_f_extdebug()
 {
     # https://superuser.com/a/974066
+    # https://www.tldp.org/LDP/abs/html/declareref.html
 
     # Turn on extended shell debugging
     shopt -s extdebug
@@ -11,3 +12,18 @@ declare_f_extdebug()
     # Turn off extended shell debugging
     shopt -u extdebug
 }
+#!/bin/bash
+
+_main(){
+    local func="${1-bash_declare_f_extdebug}"
+    bash_declare_f_extdebug "${func}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    # set -x
+    _main "$@"
+fi
+
