e29c5de1ec322f5a0b1a7a58f417e27dfbca14c3
Nathan Genetzky <nathan@genetzky.us>
2019-07-28 15:08:21 -0500

    tool/usermod: Tools to help add user to groups

---
+++ tool/usermod/usermod_docker.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+usermod_docker(){
+    local name_user
+    name_user="$(id -un)"
+    sudo usermod -aG 'docker' "${name_user}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    # set -x
+    usermod_docker "$@"
+fi
+
