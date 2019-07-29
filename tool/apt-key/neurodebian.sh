d0113ac439a8d83aacb28d62bf9e574ebee58927
Nathan Genetzky <nathan.genetzky@legrand.com>
2019-07-29 10:58:04 -0500

    tool/apt-key: Add neurodebian keys

---
+++ tool/apt-key/neurodebian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 14.04 "trusty"
+# wget -O- http://neuro.debian.net/lists/trusty.us-tn.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
+# 14.04 "trusty"
+wget -O- http://neuro.debian.net/lists/trusty.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
+sudo apt-get update
