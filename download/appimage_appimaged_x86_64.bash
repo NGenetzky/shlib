b241dc3f23f4568c3638ae753de1a066e14f125d
Nathan Genetzky <nathan@genetzky.us>
2019-07-30 07:10:44 -0500

    download: Or we could just download it.

--- download/appimage_appimaged_x86_64.bash
+++ download/appimage_appimaged_x86_64.bash
@@ -7,6 +7,6 @@ source "${SCRIPTDIR}/_download.bash"
 # Bash Strict Mode
 set -eu -o pipefail
 
-wget_download_install_executable  \
+wget_download  \
     'https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage' \
 	'appimaged-x86_64.AppImage'
