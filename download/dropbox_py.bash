531193fcd416787dae91a43418927de1a777443e
Nathan Genetzky <nathan@genetzky.us>
2019-07-30 07:46:12 -0500

    download: Download python interface for dropbox

---
+++ download/dropbox_py.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
+# shellcheck source=download/_download.bash
+source "${SCRIPTDIR}/_download.bash"
+
+# Bash Strict Mode
+set -eu -o pipefail
+
+wget_download_install_executable  \
+	'https://www.dropbox.com/download?dl=packages/dropbox.py' \
+	'dropbox.py'
