ddd1ded97df2d104efd688e2cda1d7ebe11771d8
Nathan Genetzky <nathan@genetzky.us>
2019-10-13 22:06:50 -0500

    download: Upgrade joplin

--- download/appimage_joplin.bash
+++ download/appimage_joplin.bash
@@ -7,6 +7,7 @@ source "${SCRIPTDIR}/_download.bash"
 # Bash Strict Mode
 set -eu -o pipefail
 
+VERSION='1.0.169'
 wget_download_install_executable  \
-	'https://github.com/laurent22/joplin/releases/download/v1.0.160/Joplin-1.0.160-x86_64.AppImage' \
-	'Joplin-1.0.160-x86_64.AppImage'
+	"https://github.com/laurent22/joplin/releases/download/v${VERSION}/Joplin-${VERSION}-x86_64.AppImage" \
+	"Joplin-${VERSION}-x86_64.AppImage"
