3cdecbbadf4f6281312c2d4eb0e10f02d79a39b0
Nathan Genetzky <nathan@genetzky.us>
2019-10-20 08:25:28 -0500

    utility: Add 'confirm()'

---
+++ utility/confirm.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Reference:
+# https://askubuntu.com/questions/22233/always-prompt-the-user-before-executing-a-command-in-the-shell
+# https://askubuntu.com/a/22257
+
+confirm() {
+    echo -n "Do you want to run '$*'? [N/y] "
+    read -N 1 REPLY
+    echo
+    if test "$REPLY" = "y" -o "$REPLY" = "Y"; then
+        "$@"
+    else
+        echo "Cancelled by user"
+    fi
+}
+
+_main(){
+    confirm "${@}"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    _main "$@"
+fi
+
