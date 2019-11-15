55f1a18afb7e2a24671d08fff2160d95f2f98e29
Nathan Genetzky <nathan@genetzky.us>
2019-11-14 22:41:14 -0600

    ci: Clean up test-shellcheck.bash

--- scripts/test-shellcheck.bash
+++ scripts/test-shellcheck.bash
@@ -4,19 +4,15 @@ SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 GITROOT="$(readlink -f "${SCRIPTDIR}/../")"
 
 main(){
-  (
-    cd "${GITROOT}"
-    find ./* \
-      \( -iname '*.bash' -or -iname '*.sh' \) \
-      -exec shellcheck "$@" {} +
-  )
+  cd "${GITROOT}"
+  find ./* \
+    \( -iname '*.bash' -or -iname '*.sh' \) \
+    -exec shellcheck "$@" {} +
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     # Bash Strict Mode
     set -eu -o pipefail
 
-    # set -x
     main "$@"
 fi
-
