ea5aceea2af4478c51e98ecac1be35b8fdf501ff
Nathan Genetzky <nathan@genetzky.us>
2020-11-28 21:30:41 +0000

    apt: install kvm qemu on debian 10

---
+++ tool/apt/install-kvm-qemu-on-debian10.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://computingforgeeks.com/how-to-install-kvm-virtualization-on-debian/
+sudo apt -y install \
+	qemu-kvm libvirt-daemon bridge-utils virtinst libvirt-daemon-system \
+	virt-top libguestfs-tools libosinfo-bin qemu-system
+#virt-manager
