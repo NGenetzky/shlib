10559b1408a78297e37b0295c9fa911d9771883f
Nathan Genetzky <nathan@genetzky.us>
2020-12-27 16:40:06 -0600

    snippets: Posix compliant script template

--- snippets/sh-script-template.sh
+++ snippets/sh-script-template.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 usage() {
+  PROGNAME="${PROGNAME-$0}"
   cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+Usage: ${PROGNAME} [-h] [-v] [-f] -p param_value arg1 [arg2...]
 
 Script description here.
 
@@ -17,13 +18,13 @@ EOF
 }
 
 cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
+  trap - INT TERM EXIT
   # TODO: script cleanup here
 }
 
 setup_colors() {
   # shellcheck disable=SC2034
-  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+  if [ -t 2 ] && [ -z "${NO_COLOR-}" ] && [ "${TERM-}" != "dumb" ]; then
     NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
   else
     NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
@@ -31,12 +32,12 @@ setup_colors() {
 }
 
 msg() {
-  echo >&2 -e "${1-}"
+  echo >&2 "${1-}"
 }
 
 die() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
+  msg=$1
+  code=${2-1} # default exit status 1
   msg "$msg"
   exit "$code"
 }
@@ -62,11 +63,9 @@ parse_params() {
     shift
   done
 
-  args=("$@")
-
   # check required params and arguments
-  [[ -z "${param-}" ]] && die "Missing required parameter: param"
-  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
+  [ -z "${param-}" ] && die "Missing required parameter: param"
+  [ $# -eq 0 ] && die "Missing script arguments"
 
   return 0
 }
@@ -80,15 +79,28 @@ main(){
   msg "${RED}Read parameters:${NOFORMAT}"
   msg "- flag: ${flag}"
   msg "- param: ${param}"
-  msg "- arguments: ${args[*]-}"
+  msg "- arguments: $*"
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  set -Eeuo pipefail
-  trap cleanup SIGINT SIGTERM ERR EXIT
+# POSIX-compliant way to detect if sourced.
+# https://stackoverflow.com/a/28776166/9347520
+_SCRIPT_EXECUTED=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then
+  case $ZSH_EVAL_CONTEXT in *:file) _SCRIPT_EXECUTED=1;; esac
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && _SCRIPT_EXECUTED=1
+else # All other shells: examine $0 for known shell binary filenames
+  # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|dash) _SCRIPT_EXECUTED=1;; esac
+fi
+
+if [ "${_SCRIPT_EXECUTED}" ]; then
+  # Posix Strict Mode
+  set -eu
+  trap cleanup INT TERM EXIT
 
   # shellcheck disable=SC2034 # unused
-  SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+  SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 
   main "$@"
 fi
