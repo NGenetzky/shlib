f90a16ee24396c47c80861feca1653e151744a9a
Nathan Genetzky <nathan@genetzky.us>
2021-03-12 02:34:27 -0600

    flatpak: Try out a few flatpaks

---
+++ tool/flatpak/flatpak-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+# flatpak install flathub io.neovim.nvim
+# flatpak install flathub io.github.GnomeMpv
+# flatpak install flathub com.discordapp.Discord
+
+# flatpak remove -y io.neovim.nvim
+# flatpak remove -y io.github.GnomeMpv
+# flatpak remove -y org.remmina.Remmina
+# flatpak remove -y io.github.GnomeMpv
+# flatpak remove -y md.obsidian.Obsidian
+
+# flatpak remove
