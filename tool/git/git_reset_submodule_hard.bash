9d612e83e76ee0fb660c5cac325416f44ab03936
Nathan Genetzky <nathan@genetzky.us>
2021-06-24 21:38:54 -0500

    tool/git: Use -f twice to also remove old git repos
    If the Git configuration variable clean.requireForce is not set to
    false, git clean will refuse to delete files or directories unless given
    -f, -n or -i. Git will refuse to delete directories with .git sub
    directory or file unless a second -f is given.
    
    Use force twice to clean directorise with .git subdirectories: git clean
    -xfdf. I had some tangling submodules that would not get deleted with
    just git clean -xfd.

--- tool/git/git_reset_submodule_hard.bash
+++ tool/git/git_reset_submodule_hard.bash
@@ -3,8 +3,8 @@
 # https://www.systutorials.com/how-to-reset-all-submodules-in-git/
 
 # https://gist.github.com/nicktoumpelis/11214362
-git clean -xfd
-git submodule foreach --recursive git clean -xfd
+git clean -xffd
+git submodule foreach --recursive git clean -xffd
 git reset --hard
 git submodule foreach --recursive git reset --hard
 git submodule update --init --recursive
