ada371071b0dc2083fcc98aad493b75c2dfcc9ea
Nathan Genetzky <nathan@genetzky.us>
2021-11-06 00:25:30 -0500

    tool/apt-key: neurodebian: Add debian10

--- tool/apt-key/neurodebian.sh
+++ tool/apt-key/neurodebian.sh
@@ -5,7 +5,11 @@
 # sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 
 # 14.04 "trusty"
-wget -O- http://neuro.debian.net/lists/trusty.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+# wget -O- http://neuro.debian.net/lists/trusty.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
+# debian10
+wget -O- http://neuro.debian.net/lists/buster.us-tn.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
 
 sudo apt-get update
