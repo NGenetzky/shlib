b96cb001101241beab0212af21894f5e7aa7e016
Nathan Genetzky <nathan@genetzky.us>
2022-01-23 16:12:35 -0600

    fixup! debian/xpra: Install for focal

--- provision/debian/xpra.bash
+++ provision/debian/xpra.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
+#https://github.com/Xpra-org/xpra/wiki/Download
 APT_KEY='https://xpra.org/gpg.asc'
 REPOFILE='https://xpra.org/repos/focal/xpra.list'
 
 apt install ca-certificates
 wget -q "${APT_KEY}" -O- | sudo apt-key add -
 cd /etc/apt/sources.list.d
-wget "$REPOFILE"
-apt update
-apt install xpra
+wget "$REPOFILE" -O '/etc/apt/sources.list.d/xpra.list'
+apt -yq update
+apt install --no-install-recommends xpra
