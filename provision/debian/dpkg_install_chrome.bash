b5e2a4c4f92c1c2c9fa4492d6500c5f59090c949
Nathan Genetzky <nathan@genetzky.us>
2022-02-18 18:50:31 -0600

    provision/debian: Add dpkg_install_chrome

---
+++ provision/debian/dpkg_install_chrome.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget_download_dpkg_install(){
+    local url="${1?}"
+    local name="${2?}"
+
+    DL_DIR="${DL_DIR-"${HOME}/Downloads/"}"
+    local path="${DL_DIR}/${name}"
+    wget -O "${path}" "${url}"
+    echo -e 'May need to fix broken packages using:\nsudo apt-get install -f'
+    sudo dpkg --install "${path}"
+}
+
+wget_download_dpkg_install \
+    'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' \
+    'google-chrome-stable_current_amd64.deb'
